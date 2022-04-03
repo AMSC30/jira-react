45bf062b2b92b7ec319cff02bd3f1f912776b22d
zhengxixuan <930042468@qq.com>
2022-04-03 10:21:51 +0800

    change

---
+++ src/App/app.tsx
@@ -0,0 +1,7 @@
+import './app.css'
+
+function App() {
+    return <div>111</div>
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './views/app/app'
+import App from './App/app'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
